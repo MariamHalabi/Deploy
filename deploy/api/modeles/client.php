b19b42dbb0f730e0e1a490cef16d1d5d9500748f
Mariam <mariam_halaby@hotmail.com>
2023-01-10 15:49:24 +0100

    types bdd

--- deploy/api/modeles/client.php
+++ deploy/api/modeles/client.php
@@ -1,12 +1,12 @@
 <?php
 class Client {
+    public int $clientID;
+    public string $login;
+    public string $password;
     public string $firstname;
     public string $lastname;
     public string $email;
-    public string $login;
-    public string $password;
     public string $phone;
-    public string $locale;
     public string $address;
     public string $city;
     public string $cp;
